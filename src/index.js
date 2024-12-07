94493d52f893f83a8cb558163e316679b5d828b1
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-08 01:39:28 +0545

    code some files in utils which i have to understand deeply as i am not able to understand it fully now.I will go deep inside those concepts tommorow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import "dotenv/config" 
 import connectDB from "./db/index.js"
+import app from "./app.js"
+const PORT = process.env.PORT || 3000
 
 connectDB()
+.then(
+    app.listen(PORT,()=>{
+        console.log(`Server is running at port ${PORT}`)
+    })
+)
+.catch((error)=>{
+    console.log("MONGODB CONNECTION FAILED : ", error)
+})
