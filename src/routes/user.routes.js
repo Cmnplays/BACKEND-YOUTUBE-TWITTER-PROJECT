bbd0045f98fde928b21627cc4f16eb0037c89ae4
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2024-12-14 22:36:33 +0545

    configured multer middleware in register route for accessing files

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,21 @@
 import { Router } from "express"
 const router = Router()
 import {registerUser} from "../controllers/user.controller.js"
-router.route("/register").post(registerUser)
+import {upload} from "../middlewares/multer.middleware.js"
+
+router.route("/register")
+.post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 export default router
