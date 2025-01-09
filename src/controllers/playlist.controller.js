b9a71815abf0940a5c954d5eb98270656c65c62f
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2025-01-09 14:35:52 +0545

    ADDED SOME CONTROLLERS

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,27 +1,128 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
+  if (!name.trim() || !description.trim()) {
+    throw new apiError(400, "Name and description are required");
+  }
+  const existingPlaylist = await Playlist.findOne({
+    name
+  });
+  if (existingPlaylist) {
+    //if plaulist exists set name to name +1 or 2 or the required number
+  }
+  const playlist = await Playlist.create({
+    name,
+    description,
+    videos: [],
+    owner: req.user._id
+  });
 
-  //TODO: create playlist
+  if (!playlist) {
+    throw new apiError(500, "There was a problem while creating playlist");
+  }
+
+  return res
+    .status(201)
+    .json(new apiResponse(201, playlist, "Successfully created playlist"));
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
-  //TODO: get user playlists
+  if (!userId || !isValidObjectId(userId)) {
+    throw new apiError(400, "Invalid user id");
+  }
+
+  const playlist = await Playlist.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId)
+      }
+    },
+    {
+      $addFields: {
+        numberOfVideos: {
+          $size: "$videos"
+        }
+      }
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        numberOfVideos: 1,
+        owner: 1
+      }
+    }
+  ]);
+  if (!playlist) {
+    throw new apiError(500, "There was a problem while creating playlist");
+  }
+  if (playlist.length === 0) {
+    return res
+      .status(200)
+      .json(new apiResponse(200, playlist, "No playlist created yet"));
+  }
+  return res
+    .status(200)
+    .json(new apiResponse(200, playlist, "Successfully found playlist"));
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
-  //TODO: get playlist by id
+  if (!playlistId) {
+    throw new apiError(400, "Playlist id is required");
+  }
+  const playlist = await Playlist.findById(playlistId);
+  //TODO: I NEED TO AGGREGATR VIDEOS HERE
+  if (!playlist) {
+    throw new apiError("Playlist doesn't exists");
+  }
+  return res
+    .status(200)
+    .json(new apiResponse(200, playlist, "Successfully sent playlist"));
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+  if (!playlistId || !videoId || !isValidObjectId(videoId)) {
+    throw new apiError(400, "Valid playlist id and video id is required");
+  }
+  const existingVideoInPlaylist = await Playlist.aggregate([
+    {
+      $match: {
+        videos: {
+          $in: [new mongoose.Types.ObjectId(videoId)]
+        }
+      }
+    }
+  ]);
+  if (existingVideoInPlaylist) {
+    throw new apiError(400, "Video already exists in playlist");
+  }
+  const playlist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $push: { videos: videoId }
+    },
+    {
+      new: true
+    }
+  );
+
+  if (!playlist) {
+    throw new api(500, "There was a problem while adding video to playlist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, playlist, "Successfully added video to playlist")
+    );
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
