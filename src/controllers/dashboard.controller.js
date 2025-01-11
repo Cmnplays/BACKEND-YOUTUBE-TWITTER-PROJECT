b5b1b66f38ab6e4ea46ebbd099d92925e154d41d
Aaditya Chaurasiya <chaurasiyarita20@gmail.com>
2025-01-11 12:51:33 +0545

    remaining get channel controller for now

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,17 +1,91 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
-import { Subscription } from "../models/subscription.model.js";
+import { Subscription } from "../models/subscriptions.model.js";
 import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const channelId = req.user._id;
+  const videosDetails = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(channelId)
+      }
+    }
+  ]);
+  const subscribers = await Subscription.find({
+    channel: channelId
+    //i will aggregate all subscribers here in some time with limit of 10 or 20
+  });
+  const totalLikes = await Like.aggregate([
+    {
+      video: new mongoose.Types.ObjectId(channelId)
+    }
+  ]);
+  const channelStats = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(channelId)
+      }
+    }
+  ]);
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, channelStats, "Successfully sent channel statistics")
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
+  const channelId = req.params.channelId;
+  if (!isValidObjectId(channelId)) {
+    throw new apiError(400, "Invalid channel id");
+  }
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.mongo.ObjectId(channelId)
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              email: 1,
+              avatar: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $unwind: "$owner"
+    },
+    {
+      $project: {
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        owner: 1
+      }
+    }
+  ]);
+  if (!videos) {
+    throw new apiError(400, "No videos uploaded yet");
+  }
+  return res
+    .status(200)
+    .json(new apiResponse(200, videos, "Successfully sent videos"));
 });
 
 export { getChannelStats, getChannelVideos };
